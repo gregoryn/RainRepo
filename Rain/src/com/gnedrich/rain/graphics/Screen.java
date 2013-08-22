e840bddd03c836ba655974658d308d9985bb3e37
GregoryN <gregnedrich@gmail.com>
2013-08-21 21:18:45 -0400

    Initial check in of game class and screen class.

---
+++ Rain/src/com/gnedrich/rain/graphics/Screen.java
@@ -0,0 +1,44 @@
+package com.gnedrich.rain.graphics;
+
+public class Screen {
+	
+	private int width;
+	private int height;
+	public int[] pixels;
+	
+	int xtime = 0;
+	int ytime = 0;
+	int counter = 0;
+	
+	public Screen(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height]; // 50,400 pixels
+	}
+	
+	public void clear()
+	{
+		for (int i  = 0; i < pixels.length; i++)
+		{
+			pixels[i] = 0;
+		}
+	}
+	public void render()
+	{
+		counter++;
+		if (counter % 100 == 0) xtime++;
+			if (counter % 80 == 0) ytime++;
+		
+		for (int y = 0; y < height; y++)
+		{
+			if (ytime >= height)break;
+			for (int x = 0; x < width; x++)
+			{
+				if (xtime >= width) break;
+				pixels[xtime+ytime*width] = 0xff00ff;
+			}
+		}
+	}
+
+}
